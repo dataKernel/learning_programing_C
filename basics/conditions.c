4813dd252adbe18ff700eb6216a9c3a9dea6d388
dataKernel <dataKernel@imac27.home>
2019-10-23 14:12:51 +0100

    basics::if statement

---
+++ basics/conditions.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int    main(void)
+{
+    int    age;
+    int    majority;
+    
+    age = 17;
+    majority = 18;
+
+    
+
+    if(age >= majority)
+	printf("you can enter on the site");
+    else
+	printf("go away kid");
+    
+    return 0;
+}
